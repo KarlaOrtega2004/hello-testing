a62fe6b3202a8ea0cf2e44d69423439795ba3986
Oscar Diaz <oscar01mx@gmail.com>
2026-03-03 19:43:37 -0600

    Adding initial code

--- src/main/java/com/example/hellotesting/HelloWorldController.java
+++ src/main/java/com/example/hellotesting/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.hellotesting;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,4 +16,14 @@ public class HelloWorldController {
         Thread.sleep(10000);
         return "Hello World";
     }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health()  {
+       double random = Math.random();
+       if (random < 0.2) {
+           return new ResponseEntity<>("Down", HttpStatus.SERVICE_UNAVAILABLE);
+       }
+        return new ResponseEntity<>("Healthy", HttpStatus.OK);
+    }
+
 }
